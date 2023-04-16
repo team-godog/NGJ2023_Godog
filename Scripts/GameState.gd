5ba654a4219380874ac7169e45ad16d218c0c52f
Rémi Verschelde <rverschelde@gmail.com>
2023-04-16 12:53:36 +0200

    Added: Music loop, change menu bg and splash screen

--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -5,8 +5,12 @@ var pickups_collected = []
 @export var woofs : Array[AudioStreamWAV]
 
 @onready var woofer = $BarkSFX
+@onready var music = $Music
 
 func bark():
 	woofer.stream = woofs.pick_random()
 	woofer.pitch_scale = randf_range(0.8, 1.2)
 	woofer.play()
+
+func play_music():
+	music.play()

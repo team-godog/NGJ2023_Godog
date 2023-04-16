262232f1296e374484dcfd31acab81fcd10de990
Rémi Verschelde <rverschelde@gmail.com>
2023-04-16 10:40:39 +0200

    Fixed: Herb now part of Collectible scene
    Moved VFX to the Godog scene.

--- Scripts/Collectible.gd
+++ Scripts/Collectible.gd
@@ -1,10 +1,19 @@
 extends Area3D
 
 
+var picked := false
+
+
 func _on_body_entered(body):
+	if picked:
+		return
+
 	if body is DogController:
+		picked = true
 		print("You picked me up!")
 		var tween = get_tree().create_tween()
-		tween.tween_property(self, "scale", Vector3(0.01, 0.01, 0.01), 0.4)
+		tween.tween_property(self, "scale", Vector3(0.01, 0.01, 0.01), 0.1)
+		tween.tween_property(self, "scale", Vector3(0.5, 0.5, 0.5), 0.05)
+		tween.tween_property(self, "scale", Vector3(0.01, 0.01, 0.01), 0.05)
 		tween.tween_callback(self.queue_free)
 
